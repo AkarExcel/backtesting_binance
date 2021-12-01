21693566008b7030b7e775d89a13193740924b3b
AkarExcel <therealakarexcel@gmail>
2021-12-01 03:42:44 +0100

    backtest for binance

---
+++ backtestingCpp/build/CMakeFiles/BacktestingCpp.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/MARVEL AKAR/PycharmProjects/backtesting/backtestingCpp/Database.cpp" "CMakeFiles/BacktestingCpp.dir/Database.cpp.obj" "gcc" "CMakeFiles/BacktestingCpp.dir/Database.cpp.obj.d"
+  "C:/Users/MARVEL AKAR/PycharmProjects/backtesting/backtestingCpp/Utils.cpp" "CMakeFiles/BacktestingCpp.dir/Utils.cpp.obj" "gcc" "CMakeFiles/BacktestingCpp.dir/Utils.cpp.obj.d"
+  "C:/Users/MARVEL AKAR/PycharmProjects/backtesting/backtestingCpp/strategies/Psar.cpp" "CMakeFiles/BacktestingCpp.dir/strategies/Psar.cpp.obj" "gcc" "CMakeFiles/BacktestingCpp.dir/strategies/Psar.cpp.obj.d"
+  "C:/Users/MARVEL AKAR/PycharmProjects/backtesting/backtestingCpp/strategies/Sma.cpp" "CMakeFiles/BacktestingCpp.dir/strategies/Sma.cpp.obj" "gcc" "CMakeFiles/BacktestingCpp.dir/strategies/Sma.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
